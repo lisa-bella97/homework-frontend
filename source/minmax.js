19b3aaa8886af6aaedbc8a9f6daad3ed6892835b
lisa-bella97 <lisa-bella97@mail.ru>
2019-09-01 12:20:19 +0300

    Review fixes

--- source/minmax.js
+++ source/minmax.js
@@ -1,6 +1,14 @@
 'use strict';
 
+/**
+ * Finds min and max number in the string and returns them as an array.
+ * @param {string} str
+ * @returns {[number,number]|[undefined,undefined]}
+ */
 const minmax = str => {
-  let numbers = str.split(/([^\w-.]|_|\s)+/).filter(num => num != '' && num != ' ' && !isNaN(num))
-  return numbers.length == 0 ? [ undefined, undefined ] : [ Math.min(...numbers) ].concat( Math.max(...numbers) );
+  let numbers = str
+                  .split(/([^\w-.]|_|\s)+/)
+                  .filter(num => num != '' && num != ' ' && !isNaN(num))
+  return numbers.length == 0 ? [ undefined, undefined ] : [ Math.min(...numbers) ]
+           .concat( Math.max(...numbers) );
 }
--- tests/minmax.js
+++ tests/minmax.js
@@ -34,4 +34,11 @@ QUnit.module('Тестируем функцию minmax', function () {
 		assert.deepEqual(minmax('1, -5.8 или 10, хотя 34 + -5.3 и 73'), [ -5.8, 73 ]);
 		assert.deepEqual(minmax('11,2, 4.5 и -Infinity;2'), [ -Infinity, 11 ]);
 	});
+
+	QUnit.test('minmax правильно работает с разными разделителями', function (assert) {
+		assert.deepEqual(minmax('1,1,5,-5'), [ -5, 5 ], 'Разделитель - запятая');
+		assert.deepEqual(minmax('1 3.5 2'), [ 1, 3.5 ], 'Разделитель - пробел');
+		assert.deepEqual(minmax('35678;123;1'), [ 1, 35678 ], 'Разделитель - точка с запятой');
+		assert.deepEqual(minmax('1e5, 123;45_-1 2'), [ -1, 1e5 ], 'Разные виды разделителей');
+	});
 });
